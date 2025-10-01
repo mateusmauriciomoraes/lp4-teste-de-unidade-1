ebd82a69c226be40747db608b29c47af001cff0b
mateus mauricio <153950206+mateusmauriciomoraes@users.noreply.github.com>
2025-09-30 21:55:44 -0300

    Refatoração

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,29 +1,60 @@
 public class Pessoa {
+    public double peso;
+    public double altura;
+    public String sexo;
 
-    public static void main(String[] args) {
-        System.out.println(getImc(70, 1.7, "masculino"));
-        System.out.println(getImc(70, 1.7, "feminino"));
+    public double getPeso() {
+        return peso;
     }
 
-    public static String getImc(double peso, double altura, String sexo) {
+    public void setPeso() {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura() {
+        this.altura = altura;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        if(!sexo.equals("masculino") && !sexo.equals("feminino")){
+            throw new IllegalArgumentException();
+        }
+        this.sexo = sexo;
+    }
+
+    public String getImc() {
         double imc = peso / (altura * altura);
 
         if (sexo.equals("feminino")) {
-            if (imc < 19.1) return "abaixo do peso";
-            if (imc < 25.8) return "no peso normal";
-            if (imc < 27.3) return "marginalmente acima do peso";
-            if (imc < 32.3) return "acima do peso ideal";
+            if(imc < 19.1){
+                return "abaixo do peso";
+            }else if (imc < 25.8){
+                return "no peso normal";
+            }else if (imc < 27.3){
+                return "marginalmente acima do peso";
+            }else if (imc < 32.3){
+                return "acima do peso ideal";
+            }
+            return "obeso";
+        }else{
+            if(imc < 20.7){
+                return "abaixo do peso";
+            }else if (imc < 26.4){
+                return "no peso normal";
+            }else if (imc < 27.8){
+                return "marginalmente acima do peso";
+            }else if (imc < 31.1){
+                return "acima do peso ideal";
+            }
             return "obeso";
         }
-
-        if (sexo.equals("masculino")) {
-            if (imc < 20.7) return "abaixo do peso";
-            if (imc < 26.4) return "no peso normal";
-            if (imc < 27.8) return "marginalmente acima do peso";
-            if (imc < 31.1) return "acima do peso ideal";
-            return "obeso";
-        }
-
-        return "Sexo inválido";
     }
 }
